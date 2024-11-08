d145368065e67e5f4cc1ef6c1b7ca23edaa87842
Noah Pope <npope@tutanota.com>
2024-11-08 10:07:25 -0600

    setting up background and utility keys

---
+++ Project23 - SwiftyNinja/Utilities/Constants+Utils.swift
@@ -0,0 +1,17 @@
+//
+//  Constants+Utils.swift
+//  Project23 - SwiftyNinja
+//
+//  Created by Noah Pope on 11/8/24.
+//
+
+import Foundation
+
+enum ImageKeys {
+    static let sliceBackground  = "sliceBackground"
+    static let sliceLife        = "sliceLife"
+}
+
+enum FontKeys {
+    static let chalkduster      = "Chalkduster"
+}
